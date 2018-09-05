c7b49a6ed29914d100244e4217e4c2d3a6bc2c26
Aye Thandar Aung <ayethandaraung588@gmail.com>
2018-09-05 14:12:37 +0530

    remove create

--- js/search.js
+++ js/search.js
@@ -1,10 +1,7 @@
 fb = {};
 fb = firebase;
 
-function fnCreate(path, body, callBack){
-		if(!path || !body) return;
-		fb.database().ref(path).set(body, callBack);
-}
+
 
 function fnRead(path, successFunction, errorFunction){
 		if(!path || !successFunction || !errorFunction) return;
